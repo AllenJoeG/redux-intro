72586d07935be0570fb8fd33d4fb54ba74b726db
AllenJoeG <allen.joeg@gmail.com>
2021-11-30 15:57:37 -0600

    So many notes on Redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+//New Hook way to access Reducers!
+import { useSelector, useDispatch } from 'react-redux';
+import { useState} from 'react';
 
 function App() {
+  //useSelector takes a function as an argument. That function grabs the createStore() from index.js
+  //and allows to to access the Reducers within a returned object.
+  // const reduxStore = useSelector((store) => store);
+  // console.log(reduxStore); //returns an object with count: 0, elementList: Array(4)
+
+  //Global State Reducer Fetches
+  const elementList = useSelector((store) => store.elementList);
+  const counter = useSelector((store) => store.count)
+
+  //dispatch({type: 'INSTRUCT_REDUCERS'}) must be unique, all Reducers are listening for these types
+  const dispatch = useDispatch();
+
+  const clickity = () => {
+    //dispatch function takes an object as an argument, with a 'type':'action pair {type: }
+    dispatch({type: 'INCREASE_COUNT'})
+  };
+
+  //local State for form element
+  const [elementInput, setElementInput] = useState('');
+
+  //click handler for element thingie
+  const elementConjuring = () => {
+    //should equal e.target.value of input piece
+    console.log(elementInput)
+
+    //convention when dispatching data to Reducer, call it 'payload'
+    dispatch({
+      type: 'CONJURE_ELEMENT',
+      payload: elementInput
+    })
+
+    setElementInput('');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={logo} className="App-logo" alt="logo" />
+        <h1>{counter}</h1>
+        {/* Abstract it to a higher function? NAH */}
+        <button onClick={clickity}> 👍 🔢 👍 </button>
+        {/* Call the dispatch in line! */}
+        <button onClick={() => dispatch({type: 'DECREASE_COUNT'})}> 👎 🔢 👎 </button>
+        <button onClick={() => dispatch({type: 'SCREAM_ZERO'})}> ⚡ 0️⃣ ⚡ </button>
+        
+        <div>
+          <input 
+            value={elementInput}
+            placeholder="Invent protophysics"
+            onChange={(e) => setElementInput(e.target.value)}
+
+          />
+          <button onClick={elementConjuring}>Become a God</button>
+        <ul>
+          {/* .map second argument 'i' makes an index tracker, but SHOULD be an ID in database */}
+          {elementList.map((element, i) => {
+            return <li key={i}>{element}</li>
+          })}
+        </ul>
+      </div>
+      
+      
       </header>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,82 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+
+//REDUX SETUP
+import { createStore, combineReducers, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
+
+//Import PROVIDER. Gives app a way of accessing redux, wraps the <App />
+import { Provider } from 'react-redux';
+
+//One or more reducers, Global pieces of state that live outside of React Component heirarchy
+//A function that takes two arguments, previous State, and Action
+const count = (state = 0, action) => {
+  console.log(action);
+
+  // if (action.type === 'INCREASE_COUNT') {
+  //   return state + 1;
+  // } else if(action.type === 'DECREASE_COUNT') {
+  //   return state - 1;
+  // } else if(action.type === 'SCREAM_ZERO') {
+  //   return state - state;
+  //   //return 0;
+  // }
+  // return state;
+
+  //OR
+
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return state + 1;
+    case 'DECREASE_COUNT':
+      return state - 1;
+    case 'SCREAM_ZERO':
+      return 0;
+    default: return state;
+  }
+};
+//Reducer piece of Global State
+const elementList = (state = ['oxygen', 'carbon', 'gold', 'argon'], action) => {
+  console.log(action)
+
+  switch (action.type) {
+    case 'CONJURE_ELEMENT':
+      //make an array copy of state
+      //This does not work because updateArray is a REFERENCE to state and not actually changing  
+      // let updateArray = state;
+      //so MUST use a spread operator to create actual COPY that is separate
+      const stateCopy = [...state];
+      //push payload to copy
+      stateCopy.push(action.payload)
+      //return new array with extra
+      return stateCopy;
+    default:
+      return state;
+  }
+};
+
+//One Redux store to hold Global State reducers
+//Utilize combineReducers to hold an object argument of the reducers.
+//createStore takes first argument combineReducers({}), 
+//second argument applyMiddleware(logger), both imported above, logs state updates to console
+const storeInstance = createStore(
+  combineReducers({
+    count,
+    elementList
+  }),
+  applyMiddleware(
+    logger
+  )
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* Prop must be called 'store' for everything under the hood */}
+    <Provider store={storeInstance}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +85,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
